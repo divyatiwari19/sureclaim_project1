07b31d7054c704f317cab7c493f6bc0e8c76170b
root <root@ubuntu16-04.us-east1-b.c.weighty-yew-219711.internal>
2018-12-17 20:03:36 +0000

    queries2

---
+++ DML_2_something.sql
@@ -0,0 +1,4 @@
+\c sureclaim_db1
+INSERT INTO STATION VALUES (13, 'Phoenix', 'AZ', 33, 112); 
+INSERT INTO STATION VALUES (44, 'Denver', 'CO', 40, 105); 
+INSERT INTO STATION VALUES (66, 'Caribou', 'ME', 47, 68);
